e62100058fa772aa710e3e6ee75b1c2a7dd1ca60
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-01-17 14:36:27 +0000

    Replay viewers

--- replay-plugin/src/main/kotlin/io/github/nickacpt/replay/platform/BukkitReplayPlatform.kt
+++ replay-plugin/src/main/kotlin/io/github/nickacpt/replay/platform/BukkitReplayPlatform.kt
@@ -2,15 +2,26 @@ package io.github.nickacpt.replay.platform
 
 import io.github.nickacpt.behaviours.replay.abstractions.ReplayItemStack
 import io.github.nickacpt.behaviours.replay.abstractions.ReplayPlatform
+import io.github.nickacpt.behaviours.replay.abstractions.ReplayViewer
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 
-object BukkitReplayPlatform : ReplayPlatform<ItemStack> {
+object BukkitReplayPlatform : ReplayPlatform<ItemStack, Player> {
+
+    override fun convertIntoReplayStack(stack: ItemStack): ReplayItemStack {
+        TODO("Not yet implemented")
+    }
 
     override fun convertIntoPlatformStack(stack: ReplayItemStack): ItemStack {
         TODO("Not yet implemented")
     }
 
-    override fun convertIntoReplayStack(stack: ItemStack): ReplayItemStack {
-        TODO("Not yet implemented")
+    override fun convertIntoReplayViewer(viewer: Player): ReplayViewer {
+        return BukkitReplayViewer(viewer.uniqueId)
+    }
+
+    override fun convertIntoPlatformViewer(viewer: ReplayViewer): Player? {
+        return Bukkit.getPlayer(viewer.id)
     }
 }
