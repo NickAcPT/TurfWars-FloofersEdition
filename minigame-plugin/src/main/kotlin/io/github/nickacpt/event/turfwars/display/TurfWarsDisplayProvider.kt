e621000d2325c9a595ec5c54cb7bf153bb774e19
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-01-27 01:17:00 +0000

    Display team names

--- minigame-plugin/src/main/kotlin/io/github/nickacpt/event/turfwars/display/TurfWarsDisplayProvider.kt
+++ minigame-plugin/src/main/kotlin/io/github/nickacpt/event/turfwars/display/TurfWarsDisplayProvider.kt
@@ -1,12 +1,23 @@
 package io.github.nickacpt.event.turfwars.display
 
+import io.github.nickacpt.event.core.display.PlayerDisplayData
 import io.github.nickacpt.event.core.display.PlayerDisplayProvider
 import io.github.nickacpt.event.core.display.scoreboard.PlayerScoreboardDisplayData
 import io.github.nickacpt.event.core.players.TurfPlayer
 import io.github.nickacpt.event.turfwars.minigame.TurfWarsGame.Companion.game
 import io.github.nickacpt.event.turfwars.minigame.logic.TurfWarsLogic
+import io.github.nickacpt.event.turfwars.minigame.teams.TurfWarsTeam.Companion.team
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.TextDecoration
 
 object TurfWarsDisplayProvider : PlayerDisplayProvider {
+
+    override fun provideDisplay(player: TurfPlayer): PlayerDisplayData? {
+        return player.team?.let {
+            val teamName = Component.text(it.name.uppercase(), null, TextDecoration.BOLD)
+            return PlayerDisplayData(teamName.color(it.color))
+        }
+    }
 
     override fun provideScoreboardDisplay(player: TurfPlayer): PlayerScoreboardDisplayData? {
         val game = player.game ?: return null
