e6210009c6fbdcb2c9ab94071ce9acb9d683d733
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-02-04 22:22:58 +0000

    Manually send chat message to console

--- core-plugin/src/main/kotlin/io/github/nickacpt/event/core/handlers/ChatEvents.kt
+++ core-plugin/src/main/kotlin/io/github/nickacpt/event/core/handlers/ChatEvents.kt
@@ -3,15 +3,22 @@ package io.github.nickacpt.event.core.handlers
 import io.github.nickacpt.event.core.CorePlugin
 import io.papermc.paper.chat.ChatRenderer.viewerUnaware
 import io.papermc.paper.event.player.AsyncChatEvent
+import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 
 object ChatEvents : Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOWEST)
     fun onChat(e: AsyncChatEvent) {
+        // Remove the console from the viewers, as we are going to handle it ourselves
+        e.viewers().remove(Bukkit.getConsoleSender())
+
         e.renderer(viewerUnaware { source, _, message ->
-            CorePlugin.instance.messages.chatMessage(source, message)
+            CorePlugin.instance.messages.chatMessage(source, message).also {
+                Bukkit.getConsoleSender().sendMessage(it)
+            }
         })
     }
 
