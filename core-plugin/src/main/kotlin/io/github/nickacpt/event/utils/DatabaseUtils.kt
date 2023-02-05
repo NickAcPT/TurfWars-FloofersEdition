e621000d0d335fc8f8622a4467189783a97a694b
NickAc <32451103+NickAcPT@users.noreply.github.com>
2023-02-05 00:14:39 +0000

    Add more types to extension function

--- core-plugin/src/main/kotlin/io/github/nickacpt/event/utils/DatabaseUtils.kt
+++ core-plugin/src/main/kotlin/io/github/nickacpt/event/utils/DatabaseUtils.kt
@@ -6,7 +6,17 @@ import java.sql.ResultSet
 inline operator fun <reified T> ResultSet.get(id: String): T? {
     return when (T::class.java) {
         String::class.java -> getString(id) as T
+        Boolean::class.java -> getBoolean(id) as T
+
         Int::class.java -> getInt(id) as T
+        Long::class.java -> getLong(id) as T
+
+        Short::class.java -> getShort(id) as T
+        Float::class.java -> getFloat(id) as T
+        Double::class.java -> getDouble(id) as T
+
+        Byte::class.java -> getByte(id) as T
+        ByteArray::class.java -> getBytes(id) as T
         else -> getObject(id, T::class.java)
     }.takeIf { !wasNull() }
 }
